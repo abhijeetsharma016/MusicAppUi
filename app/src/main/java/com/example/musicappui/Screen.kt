715b034c8e5ed06e6363a6fbe92d255ec6e07faf
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-03-07 02:02:36 +0530

    nav bar made for the app

--- app/src/main/java/com/example/musicappui/Screen.kt
+++ app/src/main/java/com/example/musicappui/Screen.kt
@@ -2,10 +2,12 @@ package com.example.musicappui
 
 import androidx.annotation.DrawableRes
 
+
 sealed class Screen(val title: String, val route: String) {
-    sealed class DrawerScreen(val dTitle: String, val dRute: String, @DrawableRes val icon: Int)
-        : Screen(dTitle, dRute){
-        object Account : DrawerScreen(
+
+    sealed class  DrawerScreen(val dTitle: String, val dRoute: String, @DrawableRes val icon: Int)
+        : Screen(dTitle, dRoute){
+        object Account: DrawerScreen(
             "Account",
             "account",
             R.drawable.ic_account
@@ -15,10 +17,17 @@ sealed class Screen(val title: String, val route: String) {
             "subscribe",
             R.drawable.ic_subscribe
         )
-        object AddAccount : DrawerScreen(
+
+        object AddAccount: DrawerScreen(
             "Add Account",
             "add_account",
             R.drawable.baseline_person_add_alt_1_24
-            )
-        }
+        )
+    }
 }
+
+val screensInDrawer = listOf(
+    Screen.DrawerScreen.Account,
+    Screen.DrawerScreen.Subscription,
+    Screen.DrawerScreen.AddAccount
+)
--- app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
+++ app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
@@ -1,8 +1,15 @@
 package com.example.musicappui.ui.theme
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
@@ -13,9 +20,19 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.musicappui.Screen
+import com.example.musicappui.screensInDrawer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -25,6 +42,14 @@ import kotlinx.coroutines.launch
 fun MainView() {
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     val scope: CoroutineScope = rememberCoroutineScope()
+    // Allow us to find out on which "View" we current are
+    val controller: NavController = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val title = remember{
+        mutableStateOf("")
+    }
     Scaffold(
         topBar = {
             TopAppBar(title = { Text(text = "Home") },
@@ -39,6 +64,24 @@ fun MainView() {
                     }
                 }
             )
+        }, scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp)){
+                items(screensInDrawer){
+                        item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                        if(item.dRoute == "add_account"){
+                            //open diloag
+                        }else{
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+                    }
+                }
+            }
         }
     ) {
         Text("text", modifier = Modifier.padding(it))
@@ -48,8 +91,26 @@ fun MainView() {
 @Composable
 fun DrawerItem(
     selected: Boolean,
-    onDrawerItemClicked: () -> Unit,
-    item: Screen.DrawerScreen
-) {
-
+    item: Screen.DrawerScreen,
+    onDrawerItemClicked : () -> Unit
+){
+    val background = if (selected) Color.DarkGray else Color.White
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(background)
+            .clickable {
+                onDrawerItemClicked()
+            }) {
+        Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp)
+        )
+        Text(
+            text = item.dTitle,
+            style = MaterialTheme.typography.h5,
+        )
+    }
 }
